4040183215743fe71c21809e728c825b5c5114a0
yooj-hub <jang0929@naver.com>
2021-07-10 12:21:39 +0900

    fleat: JPA STUDY 0710
    Mapping

--- hellojpa/jpa/src/main/java/hellojpa/JpaMain.java
+++ hellojpa/jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,17 +2,20 @@ package hellojpa;
 
 import javax.persistence.*;
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+        LocalDate now = LocalDate.now();
+        LocalDateTime localDateTime= LocalDateTime.now();
 
-//        Member member = new Member();
-//        member.setId(1L);
-//        member.setName("은승원");
-//        em.persist(member);
+        Member member = new Member("은승원",27,RoleType.USER,now,localDateTime,"은승원입니다.",0);
+        em.persist(member);
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
@@ -24,7 +27,7 @@ public class JpaMain {
             List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
 //            tx.commit();
             for (Member member1 : members) {
-                System.out.println("member.getName() = " + member1.getName());
+                System.out.println("member.getName() = " + member1.getUsername());
             }
             tx.commit();
         } catch (Exception e) {
--- hellojpa/jpa/src/main/java/hellojpa/Member.java
+++ hellojpa/jpa/src/main/java/hellojpa/Member.java
@@ -1,23 +1,62 @@
 package hellojpa;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import org.hibernate.type.DateType;
+
+import javax.persistence.*;
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
-@Table
 public class Member {
 
-//    @GeneratedValue
+    public Member() {
+    }
+
+    public Member(String username, Integer age, RoleType roleType, LocalDate createDate, LocalDateTime lasModifiedDate, String description, int temp) {
+        this.username = username;
+        this.age = age;
+        this.roleType = roleType;
+        this.createDate = createDate;
+        this.lasModifiedDate = lasModifiedDate;
+        this.description = description;
+        this.temp = temp;
+    }
+
+    //    @GeneratedValue
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String name;
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    private LocalDate createDate;
+
+    private LocalDateTime lasModifiedDate;
 
 
+    @Lob// 많은 내용 저장용
+    private String description;
+
+    //DB와 연결 안할려면
+    @Transient
+    private int temp;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public long getId() {
         return id;
@@ -27,11 +66,43 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getLasModifiedDate() {
+        return lasModifiedDate;
+    }
+
+    public void setLasModifiedDate(LocalDateTime lasModifiedDate) {
+        this.lasModifiedDate = lasModifiedDate;
     }
 }

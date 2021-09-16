661724dd9b1e31883a05d92ccd9c875363a2cb20
yooj-hub <jang0929@naver.com>
2021-09-16 21:22:35 +0900

    query dsl setting

---
+++ querydsl/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,133 @@
+package study.querydsl;
+
+
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.JPQLQueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslBasicTest {
+    @Autowired
+    EntityManager em;
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+
+    }
+
+    @Test
+    public void startJPQL() {
+        //member1 찾아라
+        Member findMember = em.createQuery("select m from Member m where m.username =:username", Member.class).setParameter("username", "member1").getSingleResult();
+
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+
+    @Test
+    public void startQuerydsl() {
+
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch(){
+
+//        List<Member> fetch = queryFactory.selectFrom(member).fetch();
+//        // 1개 이상일 경우 error
+//        Member fetchOne = queryFactory.selectFrom(QMember.member).fetchOne();
+//        // 처음 것만 가져옴
+//        Member fetchFirst = queryFactory.selectFrom(QMember.member).fetchFirst();
+
+        long totalCount = queryFactory.selectFrom(member).fetchCount();
+        System.out.println("totalCount = " + totalCount);
+
+        QueryResults<Member> results = queryFactory.selectFrom(member).fetchResults();
+        results.getTotal();
+        List<Member> content = results.getResults();
+
+
+
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(dsec)
+     * 2. 회원 이름 오름차순(asc)
+     * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+
+    @Test
+    public void sort(){
+        em.persist(new Member(null,100));
+        em.persist(new Member("member5",100));
+        em.persist(new Member("member6",100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        assertThat(result.get(0).getUsername()).isEqualTo("member5");
+        assertThat(result.get(1).getUsername()).isEqualTo("member6");
+        assertThat(result.get(2).getUsername()).isNull();
+
+
+    }
+
+
+
+
+}
